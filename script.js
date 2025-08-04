3dfbb934405da0aee6e37e2eebf96dbb5a2858ed
dangardiner90 <danieljosephgardiner@gmail.com>
2025-08-04 17:46:14 -0400

    Part 1 of JS

--- script.js
+++ script.js
@@ -5,16 +5,19 @@
 
 // Part 1: Understanding and creating objects
 
+// An object named student with various properties
 let student = {
     name: "Daniel",
     age: 34,
     enrolled: true,
     courses: ['JavaScript', 'Web Development', 'Database Design', 'Algorithms', 'Operating Systems'],
+    // A method within the object used to display the info
     info: function() {
         return `Student name: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}, Courses: ${this.courses}`;
     }
 };
-
+// Outputting the name, age and the info display method:
 console.log(student.name);
 console.log(student.age);
 console.log(student.info());
+
