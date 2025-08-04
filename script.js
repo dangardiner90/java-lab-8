fbf7f40dc52570120ae9b147d4a406b774ab4bd9
dangardiner90 <danieljosephgardiner@gmail.com>
2025-08-04 18:38:49 -0400

    Adding part 5 of JS

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ let student = {
     courses: ['JavaScript', 'Web Development', 'Database Design', 'Algorithms', 'Operating Systems'],
     // A method within the object used to display the info
     info: function() {
-        return `Student name: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}, Courses: ${this.courses}`;
+        console.log(`Student name: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}, Courses: ${this.courses}`);
     }
 };
 // Outputting the name, age and the info display method:
 console.log(student.name);
 console.log(student.age);
-console.log(student.info());
+student.info();
 
 
 
@@ -61,4 +61,34 @@ let newCourses = ['Advanced Frontend', 'Database Programming', 'Object Programmi
 // Combining the destructured courses property from student with the newCourses array and logging it
 let combinedCourses = { courses, ...newCourses};
 console.log(combinedCourses);
+
+
+
+// Part 5: Object methods
+
+// Copying the original student object in order to work on it
+let studentNew = {
+    name: "Daniel",
+    age: 34,
+    enrolled: true,
+    courses: ['JavaScript', 'Web Development', 'Database Design', 'Algorithms', 'Operating Systems'],
+    // A method within the object used to display the info
+    info: function() {
+        console.log(`Student name: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}, Courses: ${this.courses}`);
+    },
+    // A method to dynamically add a new course to the courses array
+    addnewCourse: function(newCourse) {
+        this.courses.push(newCourse);
+        console.log(`${newCourse} added to course list`);
+    },
+    // A method that returns the total number of courses in the array
+    totalCourses: function() {
+        return this.courses.length;
+    }
+};
+
+studentNew.addnewCourse("WRIT120");
+console.log(studentNew.courses);
+console.log(`Course totals: ${studentNew.totalCourses()}`);
+
 
